42cd3f91199a91f95765d71edc743a26718be389
Sahil Reddy <sahilpreddy96@gmail.com>
2020-06-29 11:44:10 -0500

    Update DetailsScreen.js

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,17 +1,61 @@
 import React, { Component, useState } from 'react';
-import {  View, Text, FlatList } from 'react-native';
+import {  View, Text, FlatList,TouchableOpacity, Image, StyleSheet } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 
 
-export default class Aboutscreen extends Component {
-  render() {
-    return (
-        
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>About Screen</Text>
-      </View>
-    )
+export default class DetailsScreen extends Component {
+    state = {
+        data: [
+            {
+                "name": "name",
+                "image": "https://reactnative.dev/img/tiny_logo.png"
+            },
+            {
+                "name": "name1",
+                "image": "https://reactnative.dev/img/tiny_logo.png"
+            },
+            {
+                "name": "name2",
+                "image": "https://reactnative.dev/img/tiny_logo.png"
+            }
+        ]
+    }
+
+ 
+  render(){
+      return (
+          <View style = {styles.container}>
+              <FlatList
+                data = {this.state.data}
+                renderItem = {({ item }) => (
+                    <TouchableOpacity  
+                        style={styles.stretch}>
+                        <Image style={{flex: 1}} source={{ uri:  item.image}}></Image>
+                        <Text >{item.name}</Text>
+                    </TouchableOpacity>
+                )}
+              />
+          </View>
+      );
+
   }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      paddingTop: 50,
+    },
+    stretch: {
+        padding: 10,
+        width: 150,
+        height: 150,
+        resizeMode: 'stretch',
+        flexDirection: 'row',
+    },
+    textStyle: {
+        padding: 40,
+        alignContent: 'center',
+    }
+  });
 
